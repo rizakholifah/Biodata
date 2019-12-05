d2eced80ae495a0cb2baa9e11cf12b5e580882d7
Riza Kholifah Akwan <riza.kholifah05@gmail.com>
2019-12-05 13:18:22 +0700

    Memindahkan file gitignore dan kompilasi file java

--- helloworld/helloworld.java
+++ helloworld/helloworld.java
@@ -1,5 +1,20 @@
 public class helloworld{
     public static void main(String[] args){
         System.out.println("Hello World !");
+        mobil bmw = new mobil();
+        bmw.merk = "BMW i3000";
+        bmw.maju();
+
+        mobil fordshelby = new mobil();
+        fordshelby.merk = "Fordshelby";
+        fordshelby.maju();
+    }
+}
+
+class mobil{
+    public String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" Majuu... !!! brmm....");
     }
 }
